c86f03675783f1e49707bb4e0073114d9b1a70e4
Adrien Guichard <adrien.guichard@citel.fr>
2024-05-03 02:35:14 +0200

    0.4.1-SNAPSHOT, centralize date string, use IEnumOptions, use smaller icons

--- grails-app/controllers/plm/PlmController.groovy
+++ grails-app/controllers/plm/PlmController.groovy
@@ -43,16 +43,8 @@ class PlmController implements WebAttributes {
     }
 
     def downloadPart(PlmFreeCadPart part, Long partVersion) {
-        Calendar cal = Calendar.getInstance()
-        int y = cal.get(Calendar.YEAR)
-        int m = cal.get(Calendar.MONTH)
-        int dm = cal.get(Calendar.DAY_OF_MONTH)
-        int hd = cal.get(Calendar.HOUR_OF_DAY)
-        int mn = cal.get(Calendar.MINUTE)
-        int sec = cal.get(Calendar.SECOND)
-        String date = "$y$m$dm$hd$mn$sec"
         response.contentType = 'application/zip'
-        response.setHeader("Content-disposition", "filename=\"${URLEncoder.encode("${part.originalName}${partVersion ? "-v${partVersion}" : ''}-${date}.zip", 'UTF-8')}\"")
+        response.setHeader("Content-disposition", "filename=\"${URLEncoder.encode("${part.originalName}${partVersion ? "-v${partVersion}" : ''}-${TaackUiService.dateFileName}.zip", 'UTF-8')}\"")
         response.outputStream << plmFreeCadUiService.zipPart(part, partVersion).bytes
         response.outputStream.close()
     }
